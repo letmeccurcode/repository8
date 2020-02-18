6f6537ce5e2eca63bedfaf058eb2ba1c0feb7b36
RushB <425042686@qq.com>
2020-02-18 19:31:02 +0800

    6566

--- Test2/src/itcast/A.java
+++ Test2/src/itcast/A.java
@@ -3,5 +3,13 @@ package itcast;
 public class A {
     public static void main(String[] args) {
         System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
+        System.out.println(666);
     }
 }
